d098436456a9922d125098a6f25244e060d8561d
Mário Baldé <mariobalde13@gmail.com>
2017-10-22 22:30:15 +0100

    Adicionado comentario

--- VSHelloWorld_140221056/Program.cs
+++ VSHelloWorld_140221056/Program.cs
@@ -14,6 +14,7 @@ namespace VSHelloWorld_140221056
     {
         public static void Main(string[] args)
         {
+            //Comentario de Teste
             BuildWebHost(args).Run();
         }
 
